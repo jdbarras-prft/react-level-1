c5d789896828bb4727f7c6a753660a337ee2e0fa
Jeff Barras <jeff.barras@gmail.com>
2020-03-03 13:06:55 -0600

    some changes from last ws

---
+++ reactws/ledger/src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import Transactions from './Transactions';
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="header">
+        <h1>Perficient National Bank</h1>
+      </header>
+      <Transactions />
+      {/* Ledger */}
+    </div>
+  );
+}
+
+export default App;
---
+++ reactws/ledger/src/Transactions/Deposits.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Deposits = props => (
+  <div className="transactionBox">
+    Placeholder for Deposits
+  </div>
+)
+
+export default Deposits;
